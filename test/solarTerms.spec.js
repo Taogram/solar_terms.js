5c09d6849ac55f3545ebb8c005dae6578182f21d
lax <lax263@zrtg.com>
2022-08-26 10:12:07 +0800

    update

--- test/solarTerms.spec.js
+++ test/solarTerms.spec.js
@@ -4,17 +4,20 @@
  * @Author: lax
  * @Date: 2022-06-12 09:44:10
  * @LastEditors: lax
- * @LastEditTime: 2022-08-25 23:21:58
+ * @LastEditTime: 2022-08-26 10:09:27
  * @FilePath: \tao_solar_terms\test\solarTerms.spec.js
  */
-// const SolarTerms = require("@/solarTerms.js");
-// const moment = require("moment");
+const SolarTerms = require("@/solarTerms.js");
+const moment = require("moment");
+const _2022 = require("./data/2022");
 
-// describe("year 2012", () => {
-// 	const solarTerms = new SolarTerms();
-// 	solarTerms.getSolarTermsAll(2012).map((date) => {
-// 		it(`date`, () => {
-// 			expect(date.format("YYYY-MM-DD HH:mm:ss")).toBe("-0.007032169747");
-// 		});
-// 	});
-// });
+describe("year 2022", () => {
+	const solarTerms = new SolarTerms();
+	solarTerms.getSolarTermsAll(2022).map((date, i) => {
+		const t = moment(date);
+		const str = t.format("YYYY-MM-DD HH:mm:ss");
+		it(`date ${str} is near ${_2022[i]}`, () => {
+			expect(t.isSame(_2022[i], "d")).toBe(true);
+		});
+	});
+});
