dbc57103b5209a2946aa17f37182261802a644df
lax <756993539@qq.com>
2022-08-28 10:19:53 +0800

    update

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,3 @@
-/*
- * @Description:
- * @Version: 1.0.0
- * @Author: lax
- * @Date: 2022-06-23 20:15:00
- * @LastEditors: lax
- * @LastEditTime: 2022-08-28 10:14:36
- * @FilePath: \nutationd:\program\tao\tao_solar_terms\jest.config.js
- */
 module.exports = {
 	testMatch: ["**/?(*.)(spec|test).js?(x)"],
 };
