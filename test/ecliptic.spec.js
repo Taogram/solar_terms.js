09f2d855cf3635f78be22359d1bebe637d61ef93
lax <laxlax@tutanota.com>
2023-04-25 21:37:41 +0800

    add test

--- test/ecliptic.spec.js
+++ test/ecliptic.spec.js
@@ -4,7 +4,7 @@
  * @Author: lax
  * @Date: 2022-06-12 09:44:10
  * @LastEditors: lax
- * @LastEditTime: 2022-08-25 21:59:18
+ * @LastEditTime: 2023-04-25 21:33:38
  * @FilePath: \tao_solar_terms\test\ecliptic.spec.js
  */
 const Ecliptic = require("@/ecliptic.js");
@@ -36,16 +36,70 @@ describe("《Astronomical.Algorithms》24.b jde=2448908.5 ear", () => {
 		expect(ecliptic.calcSunEclipticLongitude()).toBeCloseTo(-43.63484796, 5);
 	});
 	it(`ear sun-latitude -0.00000312`, () => {
-		expect(ecliptic.calcSunEclipticLatitude()).toBeCloseTo(-0.00000312, 5);
+		expect(ecliptic.calcSunEclipticLatitude()).toBeCloseTo(-0.00000312, 8);
 	});
 	it(`ear radius  0.99760775`, () => {
-		expect(ecliptic.sunPlanetRadius()).toBeCloseTo(0.99760775, 5);
+		expect(ecliptic.sunPlanetRadius()).toBeCloseTo(0.99760775, 8);
 	});
 	it(`ear ear-longitude  199°.907372`, () => {
 		expect(ecliptic.calcEarEclipticLongitude()).toBeCloseTo(199.907372, 3);
 	});
 	it(`ear ear-latitude  +0".644`, () => {
 		expect(ecliptic.calcEarEclipticLatitude() * 3600).toBeCloseTo(0.644, 3);
+	});
+});
+
+describe("neoprogrammics.com/vsop87 VSOP87D EARTH TEST(FULL DATA)", () => {
+	describe("jde = 2451545", () => {
+		const jde = 2451545;
+		const ecliptic = new Ecliptic(jde, { integrity: true });
+
+		it(`t 0`, () => {
+			expect(ecliptic.dt).toBe(0);
+		});
+		it(`ear sun-longitude  1.7519238681`, () => {
+			expect(ecliptic.calcSunEclipticLongitude()).toBeCloseTo(1.7519238681, 10);
+		});
+		it(`ear sun-latitude -0.0000039656`, () => {
+			expect(ecliptic.calcSunEclipticLatitude()).toBeCloseTo(-0.0000039656, 10);
+		});
+		it(`ear radius  0.9833276819`, () => {
+			expect(ecliptic.sunPlanetRadius()).toBeCloseTo(0.9833276819, 10);
+		});
+	});
+
+	describe("jde = 2415020", () => {
+		const jde = 2415020;
+		const ecliptic = new Ecliptic(jde, { integrity: true });
+
+		it(`ear sun-longitude  +1.7391225563`, () => {
+			expect(
+				(ecliptic.calcSunEclipticLongitude() % Math.PI) + Math.PI
+			).toBeCloseTo(1.7391225563, 10);
+		});
+		it(`ear sun-latitude -0.0000005679`, () => {
+			expect(ecliptic.calcSunEclipticLatitude()).toBeCloseTo(-0.0000005679, 10);
+		});
+		it(`ear radius  0.9832689778`, () => {
+			expect(ecliptic.sunPlanetRadius()).toBeCloseTo(0.9832689778, 10);
+		});
+	});
+
+	describe("jde = 2378495", () => {
+		const jde = 2378495;
+		const ecliptic = new Ecliptic(jde, { integrity: true });
+
+		it(`ear sun-longitude  1.7262638916`, () => {
+			expect(
+				(ecliptic.calcSunEclipticLongitude() % Math.PI) + Math.PI
+			).toBeCloseTo(1.7262638916, 10);
+		});
+		it(`ear sun-latitude 0.0000002083`, () => {
+			expect(ecliptic.calcSunEclipticLatitude()).toBeCloseTo(0.0000002083, 10);
+		});
+		it(`ear radius  0.9832274321`, () => {
+			expect(ecliptic.sunPlanetRadius()).toBeCloseTo(0.9832274321, 10);
+		});
 	});
 });
 
@@ -64,7 +118,7 @@ describe("《Astronomical.Algorithms》32.a jde=2448976.5 ven", () => {
 	});
 
 	it(`ear nutation offset 16".749`, () => {
-		expect(ecliptic.longitudeNutationOffset() * 3600).toBeCloseTo(16.749, 4);
+		expect(ecliptic.longitudeNutationOffset() * 3600).toBeCloseTo(16.749, 3);
 	});
 
 	it(`ear nutation offset  -1".933`, () => {
