15f46c6edbcf45f351f0446533793334e540a29c
lax <laxlax@tutanota.com>
2024-02-24 11:09:00 +0800

    update

--- readme.md
+++ readme.md
@@ -4,10 +4,11 @@
  * @Author: lax
  * @Date: 2022-02-26 14:15:25
  * @LastEditors: lax
- * @LastEditTime: 2022-08-25 23:26:56
+ * @LastEditTime: 2024-02-24 11:02:37
  * @FilePath: \tao_solar_terms\readme.md
 -->
 
 <!-- TODO -->
 
 二十四节气js实现
+vsop87d sample vs vsop87d full <60
--- test/solarTerms.spec.js
+++ test/solarTerms.spec.js
@@ -4,7 +4,7 @@
  * @Author: lax
  * @Date: 2022-06-12 09:44:10
  * @LastEditors: lax
- * @LastEditTime: 2022-08-26 10:09:27
+ * @LastEditTime: 2024-02-24 11:01:14
  * @FilePath: \tao_solar_terms\test\solarTerms.spec.js
  */
 const SolarTerms = require("@/solarTerms.js");
@@ -17,7 +17,7 @@ describe("year 2022", () => {
 		const t = moment(date);
 		const str = t.format("YYYY-MM-DD HH:mm:ss");
 		it(`date ${str} is near ${_2022[i]}`, () => {
-			expect(t.isSame(_2022[i], "d")).toBe(true);
+			expect(t.diff(_2022[i], "m") < 60).toBe(true);
 		});
 	});
 });
